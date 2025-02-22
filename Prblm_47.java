7f3b241f537e680cb5e78381ae5fa3f44b2bdd96
yogi3953 <naglayogesh1044@gmail.com>
2025-02-22 11:13:19 +0530

    remaining unsolved pattern questions

---
+++ Prblm_47.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+class Prblm_47 
+{
+    public static void main(String[]args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+         System.out.println(0);
+        for(int i=n;i>=1;i--)
+        {
+            for(int j=i;j<=n;j++)
+            {
+                System.out.print(j);
+                if(j==n)
+                {
+                    System.out.print(0);
+                    for(int k=n;k>=i;k--)
+                    {
+                        System.out.print(k);
+                    }
+                }
+            }
+            System.out.println();
+        }
+        }
+    }
---
+++ Prblm_51.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class Prblm_51 
+{
+    public static void main(String[]args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+         
+        for(int i=1;i<=n;i++)
+        {
+            
+            for(int j=i;j>=1;j--)
+            {
+                System.out.print(j+" ");
+            }
+            for (int j = 1; j <= n-i; j++) {
+                System.out.print(j+1 + " ");
+            }
+            System.out.println();
+        }
+        }
+    }
